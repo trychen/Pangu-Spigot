f7e68850971a5c28639dc57239a6c9f35a0fe42a
trychen <chenzhilins@qq.com>
2019-10-27 13:40:09 +0800

    Release 3.4

--- src/main/java/cn/mccraft/pangu/spigot/bridge/BridgeProxy.java
+++ src/main/java/cn/mccraft/pangu/spigot/bridge/BridgeProxy.java
@@ -2,15 +2,11 @@ package cn.mccraft.pangu.spigot.bridge;
 
 import cn.mccraft.pangu.spigot.Bridge;
 import cn.mccraft.pangu.spigot.PanguSpigot;
-import cn.mccraft.pangu.spigot.Remote;
 import cn.mccraft.pangu.spigot.data.Persistence;
-import cn.mccraft.pangu.spigot.server.MessageSender;
-import com.trychen.bytedatastream.ByteSerialization;
 import org.apache.commons.lang.ArrayUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.lang.invoke.MethodHandles;
 import java.lang.reflect.*;
 import java.util.Arrays;
 import java.util.Collection;
@@ -24,7 +20,6 @@ public enum BridgeProxy implements InvocationHandler {
     @SuppressWarnings("Duplicates")
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-
         if (method.isDefault()) {
             PanguSpigot.getInstance().getLogger().log(Level.SEVERE, "default method isn't support in this version", new IllegalAccessException());
             return null;
